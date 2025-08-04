74649b722ee8df2c11348ea2e6eb2119a4430b68
noahrothe <noahwilliamrothe@gmail.com>
2025-08-04 16:08:52 -0700

    Add scroll to top functionality for consistent navigation experience

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Home from './Home';
 import Menu from './Menu';
 import About from './About';
 import Contact from './Contact';
 import './App.css';
 
+// Custom hook to scroll to top on route change
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/menu" element={<Menu />} />
